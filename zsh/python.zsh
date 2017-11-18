8b4b7c68b179f22a11a31766a343f958a1b8eb5e
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2017-11-18 15:07:00 -0200

    zsh: Change python virtualenv commands to native version

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+export VIRTUALENV_DISABLE_PROMPT=1
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-__pyenv_started=0
 
-__pyenv_init() {
-  test $__pyenv_started -eq 0 && {
-    eval "$(command pyenv init --no-rehash - zsh)"
-    eval "$(command pyenv virtualenv-init -)"
-    __pyenv_started=1
-  }
-}
-
-pyenv() {
-  __pyenv_init
-  command pyenv "$@"
-}
-
-python() {
-  __pyenv_init
-  command python "$@"
-}
-
-pip() {
-  __pyenv_init
-  command pip "$@"
+venv_activate() {
+  source venv/bin/activate
 }
